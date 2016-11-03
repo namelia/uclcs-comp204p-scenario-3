1ec2a806e73443fe7710a0163abff71deaaa4db7
zijiansim123 <zijiansim123@gmail.com>
2016-11-03 03:50:58 +0000

    Added back button, delete functions, edit functions

--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -1,31 +1,48 @@
 class ReferencesController < ApplicationController
   before_action :authenticate_user!
 
-  def new
-    @reference = Reference.new
-  end
-
-
-  def index
+ def index
     @references = Reference.all
   end
 
 
+  def new
+    @reference = Reference.new
+  end
+ 
   def show
     @reference = Reference.find(params[:id])
   end
 
+  def edit
+    @reference = Reference.find(params[:id])
+  end
 
+  def update
+    @reference = Reference.find(params[:id])
+    if @reference.update(reference_params)
+      redirect_to @reference 
+    else
+      render 'edit'
+    end
+  end
 
   def create
       @reference = Reference.new(reference_params)
       if @reference.save
-        redirect_to references_path, notice: "Successfully added reference!"
+        redirect_to @reference, notice: "Successfully added reference!"
       else
         render 'new'
       end
     end
 
+   def destroy
+    @reference = Reference.find(params[:id])
+    @reference.destroy
+
+    redirect_to references_path
+  end
+  
     private
 
       def reference_params
